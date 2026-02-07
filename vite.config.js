a2d578f681c0a1e5575d55f5311deaf5e6a0a4eb
DevDev <95178561+devika-singh@users.noreply.github.com>
2026-02-07 22:56:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  base: './',       // <- This is the key for GitHub Pages
   server: {
     host: '0.0.0.0',
     hmr: {
-      clientPort: 443,
+      clientPort: 443
     }
   }
-}
+})
